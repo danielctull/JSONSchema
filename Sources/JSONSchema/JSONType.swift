e299fadedb19786d23ecd61d7bfbe64079a44299
Daniel Tull <dt@danieltull.co.uk>
2019-11-16 15:27:34 +0000

    Sort enum cases

--- Sources/JSONSchema/JSONType.swift
+++ Sources/JSONSchema/JSONType.swift
@@ -1,9 +1,9 @@
 
 public enum JSONType {
-    case string(JSONString)
     case integer(JSONInteger)
     case number(JSONNumber)
     case object(JSONObject)
+    case string(JSONString)
 }
 
 // MARK: - Equatable
@@ -19,10 +19,10 @@ extension JSONType: Decodable {
         let typeName = try container.decode(String.self, forKey: .type)
 
         switch typeName {
-        case JSONString.typeName: self = .string(try JSONString(from: decoder))
         case JSONInteger.typeName: self = .integer(try JSONInteger(from: decoder))
         case JSONNumber.typeName: self = .number(try JSONNumber(from: decoder))
         case JSONObject.typeName: self = .object(try JSONObject(from: decoder))
+        case JSONString.typeName: self = .string(try JSONString(from: decoder))
         default: throw UnknownType()
         }
     }
