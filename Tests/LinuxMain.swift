0c2b2a5c552cf91ee728ebd5023932f396d9ae68
Daniel Tull <dt@danieltull.co.uk>
2020-02-29 11:23:22 +0000

    Add GeneratorTests for Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,10 @@
 import XCTest
 
+import GeneratorTests
 import JSONSchemaTests
 
 var tests = [XCTestCaseEntry]()
+tests += GeneratorTests.__allTests()
 tests += JSONSchemaTests.__allTests()
 
 XCTMain(tests)
